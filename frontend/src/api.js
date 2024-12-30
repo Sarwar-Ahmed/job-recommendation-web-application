eaaadb71e1fbe416c04035c198425dd285bdf0bc
Sarwar-Ahmed <sarwarahmed109@gmail.com>
2024-12-30 20:08:30 +0000

    login component done

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,3 +4,4 @@ const API = axios.create({ baseURL: 'http://127.0.0.1:8000/' });
 
 export const fetchJobs = (pageNum) => API.get(`/jobs/?page=${pageNum + 1}`);
 export const recommendJobs = (skills) => API.post('/job_recommend/', { skills });
+export const logIn = (loginData) => API.post('/login', {loginData});
--- frontend/src/components/LogIn/LogIn.js
+++ frontend/src/components/LogIn/LogIn.js
@@ -1,9 +1,88 @@
-import React from 'react'
+import { Box, Button, Container, TextField, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import axios from "axios";
+import { logIn } from '../../api';
+
 
 function LogIn() {
+
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await logIn(formData);
+      localStorage.setItem("access_token", response.data.access); // Store the token
+      alert("Login Successful");
+    } catch (error) {
+      alert("Error: " + error.response.data.error);
+    }
+  };
+  
   return (
-    <div style={{witdth: "100%", textAlign: "center", height: "100vh", color: "white"}}>
-        <h1 style={{ padding: "10%" }}>Login</h1>
+    <div style={{witdth: "100%", textAlign: "center", height: "100vh"}}>
+      <Container maxWidth="xs" mt={4} sx={{ padding: '5%'}}>
+        <Box 
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            padding: 3,
+            border: '1px solid #ddd',
+            borderRadius: 2,
+            boxShadow: 2,
+          }}
+        >
+          <Typography variant="h5" component="h1" gutterBottom>
+            Login
+          </Typography>
+
+          <form onSubmit={handleSubmit} style={{ width: '100%' }}>
+            <TextField
+              label="User Name"
+              name="username"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              value={formData.username}
+              sx={{ marginBottom: 2, backgroundColor: '#ffffff', borderRadius: 2 }}
+              onChange={handleChange}
+            />
+            <TextField
+              label="Password"
+              name="password"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              type="password"
+              value={formData.password}
+              sx={{ marginBottom: 2, backgroundColor: '#ffffff', borderRadius: 2 }}
+              onChange={handleChange}
+            />
+            <Button 
+              type="submit"
+              variant="contained"
+              color="primary"
+              fullWidth
+              sx={{ mt: 2 }}
+            >
+              Log In
+            </Button>
+          </form>
+        </Box>
+      </Container>
     </div>
   )
 }
